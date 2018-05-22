d1e0020548105a065f1702761d938205960bf341
CHW <chris.chung1116@gmail.com>
2018-05-22 18:33:29 +0900

    modifiod
    the query()

--- p/mana/route.py
+++ p/mana/route.py
@@ -10,10 +10,10 @@ def home():
 @app.route("/formtest", methods=['GET','POST'])
 def formtest():
     form=StationSetUpForm()
+    stations=Station.query.all()
     if form.validate_on_submit():
     	duip=Station.query.filter_by(name=form.name.data).first()
     	if duip is None:
-	    	# st=station.query.filter_by()
 	    	station=Station(name=form.name.data,ip=form.IP_1.data)
 	    	db.session.add(station)
 	    	db.session.commit()
@@ -21,5 +21,4 @@ def formtest():
 	    	return redirect(url_for('formtest'))
     	else:
 	    	flash('data can not added','danger')
-	    	stations=Station.query.all()
     return render_template("formtest.html",form=form,title='Station',stations=stations)
